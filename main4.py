e73bcf864ca9a135354c788eeca8fd74d6863004
duadkins <duadkins@iu.edu>
2019-09-03 17:44:36 -0400

    main4 finished

--- main4.py
+++ main4.py
@@ -11,17 +11,22 @@ arcade.set_background_color(open_color.white)
 # Start the render process. This must be done before any drawing commands.
 arcade.start_render()
 
-
+#ANSWER TO THE QUESTION HERE:
+#The outer loop changes the x coordinate of the origin, shown in face_x. It increments by 150 starting at 100, going to 800
+#The inner loop has the same steps, but stops at 600 instead of 800
+#Changing the first or second numbers determines the center coordinates of the circles. If the range is extended without
+#changing the increment, more or less faces will be drawn.
+#Changing the last number determines how often a face is drawn and how close they are together
 #start at 100, go to 799, counting by 150
 for x in range(100,800,150):
         #start at 100, go to 599, counting by 150
         for y in range(100,600,150):
                 face_x,face_y = (x,y)
                 smile_x,smile_y = (face_x + 0,face_y - 0)
-                eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-                eye2_x,eye2_y = (face_x + 0,face_y + 0)
-                catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-                catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+                eye1_x,eye1_y = (face_x - 30,face_y + 50) 
+                eye2_x,eye2_y = (face_x + 30,face_y + 50)
+                catch1_x,catch1_y = (face_x - 25,face_y + 63) 
+                catch2_x,catch2_y = (face_x + 35,face_y + 63) 
 
 
                 # Draw the smiley face:
@@ -31,8 +36,9 @@ for x in range(100,800,150):
                 arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
 
                 #(x,y,width,height,color)
-                arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black)
-                arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
+                #Again, changed size of eyes to match example
+                arcade.draw_ellipse_filled(eye1_x,eye1_y,30,50,open_color.black)
+                arcade.draw_ellipse_filled(eye2_x,eye2_y,30,50,open_color.black)
                 arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2)
                 arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2)
 
